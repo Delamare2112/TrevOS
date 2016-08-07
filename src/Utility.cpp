e512bc11eb983b5f8217a14cfdf281dc1f511d37
Jackkell <bronzeeagle1@hotmail.com>
2016-08-07 13:59:12 -0700

    Removed old commented code

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -1,17 +1,5 @@
 #include "Utility.h"
 
-// Color Stuffs //
-// uint8_t NewColorShceme(enum Color fg, enum Color bg) {
-// 	return fg | bg << 4;
-// }
-//
-// uint16_t Colorfy(char c, uint8_t color) {
-// 	uint16_t c2 = c;
-// 	uint16_t color2 = color;
-// 	return c2 | color2 << 8;
-// }
-// End Color Stuffs //
-
 // strlen is in C standard but we live outside the standard so we write our own.
 size_t strlen(const char* str) {
 	size_t size = 0;
