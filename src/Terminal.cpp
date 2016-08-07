bf6cb1ebe18f354c6414dd5df71bbded558076ae
Jackkell <bronzeeagle1@hotmail.com>
2016-08-07 12:49:11 -0700

    Removed the static function from the terminal class

--- src/Terminal.cpp
+++ src/Terminal.cpp
@@ -7,21 +7,27 @@ Terminal::Terminal(size_t row, size_t column, enum Color fg, enum Color bg) {
 	this->column = column;
 	this->fg = fg;
 	this->bg = bg;
-	UpdateColorScheme();
 	ClearScreen();
 }
 
-void Terminal::UpdateColorScheme() {
-	colorShceme = fg | bg << 4;
+uint8_t Terminal::GetColorScheme() {
+	uint8_t colorShceme = fg | bg << 4;
+	return colorShceme;
 }
 
 void Terminal::ClearScreen() {
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for(size_t x = 0; x < VGA_WIDTH; x++) {
 			const size_t i = y * VGA_WIDTH + x;
-			VGA_ADDRESS[i] = Colorfy(' ', colorShceme);
+			VGA_ADDRESS[i] = Colorfy(' ', GetColorScheme());
 		}
 	}
+}
+
+uint16_t Terminal::Colorfy(char c, uint8_t color) {
+	uint16_t c2 = c;
+	uint16_t color2 = color;
+	return c2 | color2 << 8;
 }
 
 void Terminal::WriteCharAt(char c, uint8_t color, size_t x, size_t y) {
@@ -30,7 +36,7 @@ void Terminal::WriteCharAt(char c, uint8_t color, size_t x, size_t y) {
 }
 
 void Terminal::WriteChar(char c) {
-	WriteCharAt(c, colorShceme, column, row);
+	WriteCharAt(c, GetColorScheme(), column, row);
 	if(++column == VGA_WIDTH) {
 		column = 0;
 		if(++row == VGA_HEIGHT) {
@@ -49,8 +55,9 @@ void Terminal::WriteString(const char* message) {
 extern "C"
 void ForcePrintString(const char* message) {
 	size_t length = strlen(message);
-	uint8_t color = 7;
+	uint16_t color = 7;
 	for(size_t i = 0; i < length; i++) {
-		Terminal::VGA_ADDRESS[i] = Terminal::Colorfy(message[i], color);
+		uint16_t c = message[i];
+		Terminal::VGA_ADDRESS[i] = c | color << 8;
 	}
 }
--- src/include/Terminal.h
+++ src/include/Terminal.h
@@ -9,25 +9,19 @@ private:
 	size_t column;
 	enum Color fg;
 	enum Color bg;
-	uint8_t colorShceme;
 
 	static const size_t VGA_WIDTH = 80;
 	static const size_t VGA_HEIGHT = 25;
 
-	void UpdateColorScheme();
 	void ClearScreen();
+	uint16_t Colorfy(char c, uint8_t color);
 
 public:
-	// TODO: find a way to have this function not be staticwsw
-	static uint16_t Colorfy(char c, uint8_t color) {
-		uint16_t c2 = c;
-		uint16_t color2 = color;
-		return c2 | color2 << 8;
-	}
-
 	static uint16_t* const VGA_ADDRESS;
 
 	Terminal(size_t row = 0, size_t column = 0, enum Color fg = COLOR_LIGHT_GREY, enum Color bg = COLOR_BLACK);
+
+	uint8_t GetColorScheme();
 
 	void WriteCharAt(char c, uint8_t color, size_t x, size_t y);
 	void WriteChar(char c);
